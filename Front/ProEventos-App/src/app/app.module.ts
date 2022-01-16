96e4ecc09359443b396136d0d566129c98d32e7b
gaivotasmiguelito <81260058+gaivotasmiguelito@users.noreply.github.com>
2022-01-16 20:49:12 +0000

    16/01/2022

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,24 +1,51 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { ContactosComponent } from './components/contactos/contactos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { TituloComponent } from './shared/titulo/titulo.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { FormsModule } from '@angular/forms';
-import { EventoService } from './services/evento.service';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from "ngx-spinner";
+
+import { EventoService } from './services/evento.service';
+
+import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
     PalestrantesComponent,
-    NavComponent
+    ContactosComponent,
+    DashboardComponent,
+    PerfilComponent,
+    NavComponent,
+    TituloComponent,
+    DateTimeFormatPipe,
+
    ],
   imports: [
     BrowserModule,
@@ -29,10 +56,21 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     CollapseModule.forRoot(),
     FormsModule,
     FontAwesomeModule,
+    TooltipModule.forRoot(),
+    CarouselModule.forRoot(),
+    ModalModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true,
+    }),
+    NgxSpinnerModule,
   ],
   providers: [
     EventoService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
