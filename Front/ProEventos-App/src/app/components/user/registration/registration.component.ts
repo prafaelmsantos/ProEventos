a924ae8ee7427546a98f940265fcd7f4ed03d0a9
gaivotasmiguelito <81260058+gaivotasmiguelito@users.noreply.github.com>
2022-02-01 02:58:11 +0000

    01/02/2022

--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from 'src/app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup = this.formBuilder.group({});
+  // form!: FormGroup;
 
-  ngOnInit(): void {
+  get f(): any {
+    return this.form.controls;
+
+  }
+
+  constructor(private formBuilder:FormBuilder) { }
+
+  ngOnInit() {
+    this.validation();
+  }
+
+  public validation():void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('password','confirmePassword')
+    };
+
+    this.form = this.formBuilder.group(
+      {
+        primeiroNome: ['',[Validators.required]],
+        ultimoNome: ['',Validators.required],
+        email: ['',[Validators.required, Validators.email]],
+        username: ['',[Validators.required]],
+        password: ['',[Validators.required, Validators.minLength(6)]],
+        confirmePassword: ['',[Validators.required]],
+      }, formOptions
+    );
+  }
+
+  public resetForm(): void{
+    this.form.reset();
   }
 
 }
