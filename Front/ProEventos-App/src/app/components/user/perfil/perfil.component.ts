b832466b21ad96519ac229eb5a90440b4f28d014
gaivotasmiguelito <81260058+gaivotasmiguelito@users.noreply.github.com>
2022-02-02 20:24:44 +0000

    02/02/2022

--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from 'src/app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup = this.formBuilder.group({});
+  // form!: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+
+  }
+
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit() {
+    this.validation();
+  }
+
+  private validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('password', 'confirmePassword')
+    };
+
+    this.form = this.formBuilder.group({
+      titulo: ['', Validators.required],
+      primeiroNome: ['', Validators.required],
+      ultimoNome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', [Validators.required]],
+      descricao: ['', Validators.required],
+      funcao: ['', Validators.required],
+      password: ['', [Validators.minLength(6), Validators.nullValidator]],
+      confirmePassword: ['', Validators.nullValidator]
+    }, formOptions);
+  }
+
+  onSubmit(): void {
+
+    // Vai parar aqui se o form estiver inválido
+    if (this.form.invalid) {
+      return;
+    }
+  }
+
+  public resetForm(event: any): void {
+    event.preventDefault();
+    this.form.reset();
   }
 
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ProEventosApp - Rafael
+# ProEventos-App - Rafael
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.2.
 
